2385a4e882556dc77d27a56e2f41811f17c3e222
Sanjay-S07 <sureshsanjay033@gmail.com>
2024-10-26 14:58:33 +0530

    Update config.prod.js

--- config/config.prod.js
+++ config/config.prod.js
@@ -4,5 +4,5 @@ window.config = {
     bio: "#{USER_BIO}#",
     location: "#{USER_LOCATION}#",
     email: "#{USER_EMAIL}#",
-    image: "sans/p1.jpeg"
+    image: "https://th.bing.com/th?id=OIP.Rr6U6Wu5mgtHD_FsxcVQlAHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
   };
